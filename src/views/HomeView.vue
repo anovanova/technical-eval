ce069461148aec13625cd04565080a53c2dfdfb3
anovanova <acederamichael07@gmail.com>
2023-08-19 02:50:01 +0800

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<script setup>
+import axios from 'axios'
+
+</script>
+
+<script>
+axios.get("https://netzwelt-devtest.azurewebsites.net/Territories/All")
+  .then((response) => {
+    console.log(response.message);         
+  });
+</script>
+
+<template>
+  <main>
+
+  </main>
+</template>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,56 @@
+<script setup>
+import axios from 'axios'
+</script>
+<script>
+export default {
+    data() {
+        return{
+            username: '',
+            password: ''
+        }
+        
+    },
+    methods: {
+        async sendData() {
+            const headers = {
+                'Content-Type': 'application/json',
+            };
+           
+            axios.post("https://netzwelt-devtest.azurewebsites.net/Account/SignIn",{headers}, {
+                username: this.username,
+                password: this.password
+            })
+            .then((response) => {
+                if(response.message !== 'undefined'){
+                    alert(response.message);
+                }
+                else{
+                    this.$router.push('/home')
+                }
+            });
+        }
+    }
+}
+</script>
+
+<template>
+  <main>
+    <div class="container">
+        <div class="login-items-container">
+            <div>
+                <input id="username" type="text" v-model="username">
+            </div>
+            <div>
+                <input id="password" type="password" name="" v-model="password">
+            </div>
+            <div>
+                <button id="submit" class="button" @click="sendData">Login</button>
+            </div>
+        </div>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
